ed78aa5d66ceedeb43fbd49eb6dc4b63a24a361d
Eric Chen <echen0719@gmail.com>
2026-07-05 16:25:15 -0400

    Added scanning functionality and keybind
    It now scans all the wanted chunks for the block when you press "v". It seems to work pretty fast. May need to update a little bit in the future.

--- src/client/java/echen0719/blockfinder/client/BlockFinderClient.java
+++ src/client/java/echen0719/blockfinder/client/BlockFinderClient.java
@@ -1,10 +1,34 @@
 package echen0719.blockfinder.client;
 
+import net.minecraft.client.KeyMapping;
+import net.minecraft.world.level.block.Blocks;
+
+import com.mojang.blaze3d.platform.InputConstants;
+
+import org.lwjgl.glfw.GLFW;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 
 public class BlockFinderClient implements ClientModInitializer {
+	public static KeyMapping scanKey;
+
+	private static final KeyMapping.Category category = null;
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		scanKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+			"key.blockfinder.scan", 
+			InputConstants.Type.KEYSYM, 
+			GLFW.GLFW_KEY_V,
+			category
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (scanKey.consumeClick()) {
+				BlockScanner.scan(7, Blocks.DIAMOND_ORE);
+			}
+		});
 	}
 }
--- src/client/java/echen0719/blockfinder/client/BlockScanner.java
+++ src/client/java/echen0719/blockfinder/client/BlockScanner.java
@@ -1,7 +1,9 @@
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.core.BlockPos;
+package echen0719.blockfinder.client;
 
-import net.minecraft.world.level.Level;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -11,42 +13,85 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.CompletableFuture;
 
 public class BlockScanner {
-    public static final List<BlockPos> foundBlocks = Collections.synchronizedList(new ArrayList<>()); // for concurrent scanning
+    private static final Minecraft client = Minecraft.getInstance();
 
-    private static int chunkRadius = 62;
-    private static Blocks wantedBlock = Blocks.DIAMOND_ORE;
+    public static List<BlockPos> foundBlocks = Collections.synchronizedList(new ArrayList<>()); // for concurrent scanning safety
 
     private static boolean isScanning = false;
+    private static ExecutorService executor = Executors.newCachedThreadPool();
 
-    public static void scan(MinecraftClient client) {
+    public static void scan(int chunkRadius, Block targetBlock) {
+        System.out.print("Started the scan!");
+
         if (isScanning || client.level == null || client.player == null) {
+            System.out.print("Ended the scan!");
             return;
         }
 
         isScanning = true;
         foundBlocks.clear();
 
-        BlockPos playerCenter = client.player.getBlockPos();
-        int playerCenterChunkX = playerPos.getX() >> 4;
-        int playerCenterChunkZ = playerPos.getZ() >> 4;
+        BlockPos playerCenter = client.player.blockPosition();
+        int playerCenterChunkX = playerCenter.getX() >> 4;
+        int playerCenterChunkZ = playerCenter.getZ() >> 4; // seems like 4 bit shift is better than /16
 
         List<LevelChunk> chunksToScan = new ArrayList<>();
 
         for (int x = -chunkRadius; x <= chunkRadius; x++) {
             for (int z = -chunkRadius; z <= chunkRadius; z++) {
-                LevelChunk chunk = Level.getChunk(playerCenterChunkX + x, playerCenterChunkZ + z);
-                if (chunk != null) {
+                LevelChunk chunk = client.level.getChunk(playerCenterChunkX + x, playerCenterChunkZ + z);
+                if (chunk != null && !chunk.isEmpty()) {
                     chunksToScan.add(chunk);
                 }
             }
         }
 
-        CompletableFuture.runAsync(() -> {
+        executor.submit(() -> {
+            for (LevelChunk chunk : chunksToScan) {
+                scanInChunk(chunk, targetBlock);
+            }
 
+            client.execute(() -> {
+                for (BlockPos position : foundBlocks) {
+                    double x = position.getX() + 0.5;
+                    double y = position.getY() + 0.5;
+                    double z = position.getZ() + 0.5;
+
+                    while (true) {
+                        try {Thread.sleep(300);} catch (Exception e) {}
+                        client.level.addParticle(ParticleTypes.TOTEM_OF_UNDYING, x, y, z, 0.0, 0.0, 0.0);
+                    }
+                }
+            });
+
+            isScanning = false;
+        });
+    }
+
+    public static void scanInChunk(LevelChunk chunk, Block targetBlock) {
+        int chunkMinX = chunk.getPos().getMinBlockX();
+        int chunkMinZ = chunk.getPos().getMinBlockZ();
+
+        // the docs say that this is better when performing many computations at a time
+        BlockPos.MutableBlockPos position = new BlockPos.MutableBlockPos();
+
+        for (int y = -64; y < 320; y++) { // 1.18+ versions for now
+            for (int x = 0; x < 16; x++) {
+                for (int z = 0; z < 16; z++) {
+                    position.set(chunkMinX + x, y, chunkMinZ + z);
+                    BlockState state = chunk.getBlockState(position);
+
+                    if (state.is(targetBlock)) {
+                        foundBlocks.add(position.immutable()); // world position and thread safe?
+                    }
+                }
+            }
         }
+
+        System.out.println("Done for " + chunk.getPos().x() + ", " + chunk.getPos().z() + "!");
     }
 }
