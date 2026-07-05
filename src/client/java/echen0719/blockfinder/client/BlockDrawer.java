67fb105595a4a77ad12b9e8d884e417dbdfa1a18
Eric Chen <echen0719@gmail.com>
2026-07-05 19:12:57 -0400

    Update BlockDrawer.java
    Simplified version. I reduced the number of values you needed by drawing edges instead of faces or something like that. I also fixed a few bugs with the matrix pose and push thingy.

--- src/client/java/echen0719/blockfinder/client/BlockDrawer.java
+++ src/client/java/echen0719/blockfinder/client/BlockDrawer.java
@@ -25,45 +25,59 @@ public class BlockDrawer {
         PoseStack matrices = context.poseStack(); // i assume this is close to CFrames in Roblox
         if (matrices == null) return;
 
+        Matrix4f matrix = matrices.last().pose(); // don't know what this does
+
         // calculate block postion relative to the camera
-        matrices.pushPose();
-        matrices.translate(
-            position.getX() - cameraPosition.x, 
-            position.getY() - cameraPosition.y, 
-            position.getZ() - cameraPosition.z
-        );
+        float dx = (float)(position.getX() - cameraPosition.x);
+        float dy = (float)(position.getY() - cameraPosition.y);
+        float dz = (float)(position.getZ() - cameraPosition.z);
 
         context.submitNodeCollector().submitCustomGeometry(matrices, RenderTypes.lines(), (renderer, vertex) -> {
-            Matrix4f matrix = matrices.last().pose(); // don't know what this does
-
             float offset = 0.002f;
-            float x1 = 0.0f - offset, x2 = 1.0f + offset;
-            float y1 = 0.0f - offset, y2 = 1.0f + offset;
-            float z1 = 0.0f - offset, z2 = 1.0f + offset;
+            float x1 = dx - offset;
+            float x2 = dx + 1.0f + offset;
+
+            float y1 = dy - offset;
+            float y2 = dy + 1.0f + offset;
+
+            float z1 = dz - offset;
+            float z2 = dz + 1.0f + offset;
 
             float r = 1.0f;
             float g = 0.0f; 
             float b = 0.0f;
             float a = 0.5f; // using wildbeast42's values
 
-            drawEdge(vertex, matrix, x1, y1, z1, x2, y1, z1, x2, y1, z2, x1, y1, z2, r, g, b, a); // bottom
-            drawEdge(vertex, matrix, x1, y2, z1, x1, y2, z2, x2, y2, z2, x2, y2, z1, r, g, b, a); // top
-            drawEdge(vertex, matrix, x1, y1, z1, x1, y2, z1, x2, y2, z1, x2, y1, z1, r, g, b, a); // north
-            drawEdge(vertex, matrix, x1, y1, z2, x2, y1, z2, x2, y2, z2, x1, y2, z2, r, g, b, a); // south
-            drawEdge(vertex, matrix, x1, y1, z1, x1, y1, z2, x1, y2, z2, x1, y2, z1, r, g, b, a); // west
-            drawEdge(vertex, matrix, x2, y1, z1, x2, y2, z1, x2, y2, z2, x2, y1, z2, r ,g, b, a); // east
-        });
+            // cuz ya know, a cube is 12 edges and 6 faces...3rd grade stuff
+            drawEdge(vertex, matrix, x1, y1, z1, x2, y1, z1, r, g, b, a); // Bottom-North
+            drawEdge(vertex, matrix, x1, y1, z2, x2, y1, z2, r, g, b, a); // Bottom-South
+            drawEdge(vertex, matrix, x1, y1, z1, x1, y1, z2, r, g, b, a); // Bottom-West
+            drawEdge(vertex, matrix, x2, y1, z1, x2, y1, z2, r, g, b, a); // Bottom-East
 
-        matrices.popPose();
+            drawEdge(vertex, matrix, x1, y2, z1, x2, y2, z1, r, g, b, a); // Top-North
+            drawEdge(vertex, matrix, x1, y2, z2, x2, y2, z2, r, g, b, a); // Top-South
+            drawEdge(vertex, matrix, x1, y2, z1, x1, y2, z2, r, g, b, a); // Top-West
+            drawEdge(vertex, matrix, x2, y2, z1, x2, y2, z2, r, g, b, a); // Top-East
+
+            drawEdge(vertex, matrix, x1, y1, z1, x1, y2, z1, r, g, b, a); // Side-North
+            drawEdge(vertex, matrix, x2, y1, z1, x2, y2, z1, r, g, b, a); // Side-East
+            drawEdge(vertex, matrix, x1, y1, z2, x1, y2, z2, r, g, b, a); // Side-West
+            drawEdge(vertex, matrix, x2, y1, z2, x2, y2, z2, r, g, b, a); // Side-East
+        });
     }
 
     public static void drawEdge(VertexConsumer vertex, Matrix4f matrix, 
-    float x1, float y1, float z1, float x2, float y2, float z2, 
-    float x3, float y3, float z3, float x4, float y4, float z4, 
-    float r, float g, float b, float a) { // draws from vertices
-        vertex.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a);
-		vertex.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a);
-		vertex.addVertex(matrix, x3, y3, z3).setColor(r, g, b, a);
-		vertex.addVertex(matrix, x4, y4, z4).setColor(r, g, b, a);
+    float x1, float y1, float z1, float x2, float y2, float z2,
+    float r, float g, float b, float a) {
+        
+        float nx = x2 - x1;
+        float ny = y2 - y1;
+        float nz = z2 - z1;
+        
+        float lineWidth = 2.0f;
+
+        // start & end
+        vertex.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a).setNormal(nx, ny, nz).setLineWidth(lineWidth);
+        vertex.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a).setNormal(nx, ny, nz).setLineWidth(lineWidth);
     }
 }
