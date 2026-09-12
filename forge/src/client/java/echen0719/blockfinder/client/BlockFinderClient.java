e3219ee2722b4dfc60fe0239757cbdfa428c7201
Eric Chen <echen0719@gmail.com>
2026-09-12 18:04:56 -0400

    Small show HUD checkbox bug
    Changed

--- forge/src/client/java/echen0719/blockfinder/client/BlockFinderClient.java
+++ forge/src/client/java/echen0719/blockfinder/client/BlockFinderClient.java
@@ -148,6 +148,6 @@ public class BlockFinderClient {
 	} // not sure why this is called "Chat" but you know...if it works, it works :D
 
 	public static void showHUD() {
-        HUDInfo.showHUD = true;
+        // HUDInfo.showHUD = true;
     }
 }
